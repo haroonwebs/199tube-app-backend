7f3b34f245ba1903ff5353dfcb1ba4f443da4334
haroontalib <haroontalib8029@gmail.com>
2026-02-02 01:59:59 +0500

    dfljsalk

--- src/index.ts
+++ src/index.ts
@@ -14,8 +14,6 @@ app.use(
   cors({
     origin: process.env.ALLOW_FRONTEND_ORIGIN,
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // allow these methods
-    allowedHeaders: ["Content-Type", "Authorization", "Accept"],
   })
 );
 app.use(express.urlencoded({ extended: true }));
