1ae57b05137f8e660847d5be599fb73e423adcc4
haroontalib <haroontalib8029@gmail.com>
2026-02-02 22:47:26 +0500

    again changed env

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ const port = process.env.PORT || 4000;
 
 app.use(
   cors({
-    origin: process.env.ALLOW_FRONTEND_ORIGIN,
+    origin: "https://199tube-intertainment.vercel.app",
     credentials: true,
   })
 );
